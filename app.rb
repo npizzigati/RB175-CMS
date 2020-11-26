a230174658dee0339e4626fefd21d1e89be1729c
Nicholas Pizzigati <npizzigati@gmail.com>
2020-11-26 12:07:49 -0600

    Implementation of all assignments up to "Viewing Text Files"

--- app.rb
+++ app.rb
@@ -1,7 +1,35 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'tilt/erubis'
-require 'sinatra/development'
+require 'sinatra/reloader'
+
+set :erb, :escape_html => true
+
+USER_FILES_DIRECTORY = 'user_files'.freeze
+
+helpers do
+end
+
+def user_files_path(file = nil)
+  root = File.expand_path(__dir__)
+  path = File.join(root, USER_FILES_DIRECTORY)
+  path = File.join(path, file) if file
+  path
+end
+
+def retrieve_filenames
+  files = []
+  Dir.open(user_files_path) do |entry|
+    files << entry if Dir.exist? entry
+  end
+end
 
 get '/' do
+  @filenames = retrieve_filenames
   erb :start
+end
+
+get '/:filename' do
+  @lines = File.readlines(user_files_path(params[:filename]))
+  erb :file_text
 end
